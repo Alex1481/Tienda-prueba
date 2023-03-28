fc59eb92aad7f10c96d09cdde60934fff679aec5
“Michael” <infoalexumg@gmail.com>
2023-03-27 21:17:01 -0600

    Creando repositorio

---
+++ Config/Config.php
@@ -0,0 +1,21 @@
+<?php 
+	//define("BASE_URL", "http://localhost/MEGA_BIT/");
+	const BASE_URL = "http://localhost/tienda_virtual2/";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+
+	//DATOS DE CONEXIÓN A BASE DE DATOS
+	const DB_HOST = "localhost";
+	const DB_NAME = "tienda";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "charset=utf8";
+
+	//Delimitadores decimal y millar Ej. 25,999.99
+	const SPD = ".";
+	const SPM = ",";
+
+	//Símbolo de moneda
+	const SMONEY = "Q";
+ ?>
---
+++ README.md
@@ -0,0 +1,17 @@
+# MEGA BIT
+Proyecto para el CiberCafé Mega Bit
+
+## Proyecto PHP con Conexión a MySQL
+
+### Datos de MySQL a utilizar:
+
+user: root
+pass: 
+
+Nota: Sin contraseña
+
+## Base de Datos y Model:
+
+### Base de Datos Solo de Peliculas, Noticias, Banners.
+
+https://drive.google.com/drive/folders/1Rw9KMNfM3kIfSjukt27VnB_MV_iSpejg?usp=sharing
